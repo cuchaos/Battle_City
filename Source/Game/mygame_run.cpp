bb361ed2d8aa21c5d9e9945cc9f8c1f57361cc25
garry911128 <garry80633@gmail.com>
2023-04-08 21:08:51 +0800

    Ship Item

--- Source/Game/mygame_run.cpp
+++ Source/Game/mygame_run.cpp
@@ -180,7 +180,7 @@ void CGameStateRun::OnInit()                                  // 遊戲的初值
 					,{ 4, 4, 4, 4, 5, 1, 1, 1, 1, 1, 1, 4, 7, 7, 4, 1, 1, 1, 4, 4, 1, 1, 4, 4, 1, 1 }//25
 	};
 	
-	Stage1.OnInit(tempstage1);
+	Stage1.OnInit(tempstage5);
 	
 	_MouseX = 0;
 	_MouseY = 0;

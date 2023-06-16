18af100d3bc8e758fec7a83cd6e1bf51ed0d7554
garry911128 <garry80633@gmail.com>
2023-06-16 19:27:11 +0800

    final

--- Source/Game/mygame_run.cpp
+++ Source/Game/mygame_run.cpp
@@ -300,13 +300,13 @@ void CGameStateRun::OnShowText() {
 
 	_Menu.OnShowText(pDC, fp);
 	CTextDraw::ChangeFontLog(pDC, 15, "STZhongsong", RGB(255, 255, 255));
-	CTextDraw::Print(pDC, 0, 90, ("EnemyNums:" + to_string(_EnemyExistNum)));
+	/*CTextDraw::Print(pDC, 0, 90, ("EnemyNums:" + to_string(_EnemyExistNum)));
 	CTextDraw::Print(pDC, 0, 110, ("PlayerLife(remaing Respawntimes):" + to_string(_PlayerRespawnTimes)));
 	CTextDraw::Print(pDC, 0, 130, ("Bullet Position:" + to_string(_AllBullet[0]->GetNowFrontPlace()[0][0]) +","+ to_string(_AllBullet[0]->GetNowFrontPlace()[0][1])));
 	CTextDraw::Print(pDC, 0, 150, ("PlayerAngle:" + to_string(_PlayerTank.GetOriginAngle())));
 	CTextDraw::Print(pDC, 0, 170, ("PlayerBitmapIndex:" + to_string(_PlayerTank.GetBitmapIndex())));
 	CTextDraw::Print(pDC, 0, 190, ("PlayerHoldKey:" + to_string(_HoldKey)));
-	CTextDraw::Print(pDC, 0, 210, ("PlayerScore:" + to_string(_NowTotalScore)));
+	CTextDraw::Print(pDC, 0, 210, ("PlayerScore:" + to_string(_NowTotalScore)));*/
 	CTextDraw::Print(pDC, 0, 500, ("Press L Add RespawnTimes"));
 	CTextDraw::Print(pDC, 0, 520, ("Press A Jump to Next Stage"));
 	CTextDraw::Print(pDC, 0, 540, ("Press D Respawn"));
